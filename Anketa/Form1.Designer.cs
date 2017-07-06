5f32dc388cc69a57b0b77e81e873927f3aed070c
Ivan Shcherbyna <shcherbyna.ivan@gmail.com>
2017-07-06 22:20:30 +0300

    Add files via upload

---
+++ Anketa/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Anketa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbSurname = new System.Windows.Forms.TextBox();
+            this.tbFathersName = new System.Windows.Forms.TextBox();
+            this.tbGoroskop = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lbSex = new System.Windows.Forms.ListBox();
+            this.dtpBithDate = new System.Windows.Forms.DateTimePicker();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(85, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Заполни свою анкету";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Фамилия";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Отчество";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "дата рождения";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "пол";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(14, 185);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "гороскоп";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(98, 40);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(189, 20);
+            this.tbName.TabIndex = 1;
+            // 
+            // tbSurname
+            // 
+            this.tbSurname.Location = new System.Drawing.Point(98, 67);
+            this.tbSurname.Name = "tbSurname";
+            this.tbSurname.Size = new System.Drawing.Size(189, 20);
+            this.tbSurname.TabIndex = 1;
+            // 
+            // tbFathersName
+            // 
+            this.tbFathersName.Location = new System.Drawing.Point(98, 93);
+            this.tbFathersName.Name = "tbFathersName";
+            this.tbFathersName.Size = new System.Drawing.Size(189, 20);
+            this.tbFathersName.TabIndex = 1;
+            // 
+            // tbGoroskop
+            // 
+            this.tbGoroskop.Location = new System.Drawing.Point(98, 185);
+            this.tbGoroskop.Name = "tbGoroskop";
+            this.tbGoroskop.Size = new System.Drawing.Size(189, 20);
+            this.tbGoroskop.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(110, 225);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lbSex
+            // 
+            this.lbSex.FormattingEnabled = true;
+            this.lbSex.Location = new System.Drawing.Point(98, 152);
+            this.lbSex.Name = "lbSex";
+            this.lbSex.Size = new System.Drawing.Size(97, 30);
+            this.lbSex.TabIndex = 3;
+            // 
+            // dtpBithDate
+            // 
+            this.dtpBithDate.Location = new System.Drawing.Point(98, 123);
+            this.dtpBithDate.Name = "dtpBithDate";
+            this.dtpBithDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpBithDate.TabIndex = 4;
+            this.dtpBithDate.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(15, 225);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnClear.Size = new System.Drawing.Size(89, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(207, 225);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(89, 23);
+            this.btnOk.TabIndex = 6;
+            this.btnOk.Text = "Готово";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 267);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.dtpBithDate);
+            this.Controls.Add(this.lbSex);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbGoroskop);
+            this.Controls.Add(this.tbFathersName);
+            this.Controls.Add(this.tbSurname);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Приложение Анкета";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbSurname;
+        private System.Windows.Forms.TextBox tbFathersName;
+        private System.Windows.Forms.TextBox tbGoroskop;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ListBox lbSex;
+        private System.Windows.Forms.DateTimePicker dtpBithDate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
+
---
+++ Anketa/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Anketa
+{
+    
+
+    public partial class Form1 : Form
+    {
+        private SaveFileDialog save;
+        private string filename;
+        StreamWriter sw;
+        private string anketText;
+        string error = "Заполните все поля";
+        public Form1()
+        {
+            string helloText = "Приветствую!\nХотите заполнить анкету?";
+            if (MessageBox.Show(helloText, "Приветствие", MessageBoxButtons.YesNo) != DialogResult.No)
+            {
+                InitializeComponent();
+                lbSex.Items.Insert(0,"Мужщина");
+                lbSex.Items.Insert(0,"Женщина");
+            }
+            else this.Close();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private string getAnket()
+        {
+            
+            //if (!string.IsNullOrEmpty(lbSex.Text) || !string.IsNullOrEmpty(tbSurname.Text) || !string.IsNullOrEmpty(tbName.Text) || !string.IsNullOrEmpty(tbGoroskop.Text) || !string.IsNullOrEmpty(tbFathersName.Text))
+            //{
+
+                anketText = "Анкета: \n" + tbSurname.Text + " " + tbName.Text + " " + tbFathersName.Text + "\nПол: " + lbSex.SelectedItem + "\nВозраст: " + dtpBithDate.Value.Date + "\n" + tbGoroskop.Text + " знака по гороскопу.";
+                return anketText;
+            //}
+            //else return error;
+        }
+        public void onSave(String texttoSave)
+        {
+            try
+            {
+                save = new SaveFileDialog();
+                save.Filter = "Text files|*.txt|All files|*.*";
+                save.ShowDialog();
+                sw = new StreamWriter(save.FileName);
+                this.filename = save.FileName;
+                sw.Write(texttoSave);
+                sw.Flush();
+                sw.Close();
+
+                                    
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message);}
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbName.Text = "";
+            tbSurname.Text = "";
+            tbFathersName.Text = "";
+            tbGoroskop.Text = "";
+            dtpBithDate.Text = "";
+            lbSex.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                onSave(getAnket());
+            }
+            catch (Exception ex) { MessageBox.Show(error);}
+            }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {try
+            {
+                // if(!string.IsNullOrEmpty(lbSex.Text)|| !string.IsNullOrEmpty(tbSurname.Text)|| !string.IsNullOrEmpty(tbName.Text)|| !string.IsNullOrEmpty(tbGoroskop.Text)||!string.IsNullOrEmpty(tbFathersName.Text)) MessageBox.Show(getAnket());
+                //else MessageBox.Show("Заполните все поля!");
+                MessageBox.Show(getAnket());
+            }
+            catch (Exception ex) { MessageBox.Show(error);}
+        }
+    }
+}
+
